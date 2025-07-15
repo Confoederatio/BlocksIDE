4de42365762e4f9f86ad7f90e6583472c9f84238
Confoederatio <htmlprogrammingbn@gmail.com>
2025-07-15 23:48:13 +0100

    added functional info display

--- src/js/ui/ide_information.js
+++ src/js/ui/ide_information.js
@@ -19,4 +19,42 @@
 			file_information_el.innerHTML = current_file.join("/");
 		}, 1000);
 	}
+
+	function printInformationPanel () {
+		//Close UI if it already exists
+		if (window.information_ui) window.information_ui.close();
+
+		window.information_ui = new ve.Window({
+			can_rename: false,
+			id: "information-ui",
+			name: "Information",
+			x: 100,
+			y: 200,
+
+			interface: {
+				display_info: {
+					id: "display_info",
+					innerHTML: "Loading ..",
+					type: "html",
+					onload: (e) => {
+						window.information_ui_info_loop = setInterval(function(){
+							//Clear interval and break if UI doesn't exist
+							if (!document.querySelector(`#information-ui`)) {
+								clearInterval(window.information_ui_info_loop);
+							} else {
+								var display_string = [];
+
+								//Format display_string
+								display_string.push(`<b>Project Folder:</b> ${path.join(window.main.saves_folder)}`);
+								display_string.push("");
+								display_string.push(`<b>Current File:</b> ${path.join((window.current_file) ? window.current_file : "(none)")}`)
+
+								e.innerHTML = display_string.join("<br>");
+							}
+						}, 500);
+					}
+				}
+			}
+		});
+	}
 }
